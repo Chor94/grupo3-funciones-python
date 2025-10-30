6596bc97f450ae1671a57bdc9643ed2528cb6566
Franco Nicolas Villegas <villegasfranconicolas@gmail.com>
2025-10-30 17:02:32 -0300

    Fix import path for promedio function in tests

--- tests/test_promediovillegas.py
+++ tests/test_promediovillegas.py
@@ -1,5 +1,5 @@
 #tests/test_promedio.py
-from funciones.promedio-villegas import promedio
+from funciones.promediovillegas import promedio
 def test_promedio():
  assert promedio([2, 4, 6]) == 4
  assert promedio([]) is None
