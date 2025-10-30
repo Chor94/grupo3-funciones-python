19aea7c729ca0cea1906ac8dec1e39b99ce4aa28
Franco Nicolas Villegas <villegasfranconicolas@gmail.com>
2025-10-30 17:01:33 -0300

    Delete test_aguiar.py

--- test_aguiar.py
+++
@@ -1,6 +0,0 @@
-from funciones.aguiar import multiplicar_aguiar
-
-def test_multiplicar_aguiar():
-    assert multiplicar_aguiar(3, 4) == 12
-    assert multiplicar_aguiar(-2, 5) == -10
-    assert multiplicar_aguiar(0, 5) == 0
